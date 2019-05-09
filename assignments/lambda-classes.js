e7f9f8a4a419ca18de65a71e42b6b5ce4d0988aa
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-09 12:26:38 +0100

    Basic class structure added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,16 @@
 // CODE here for your Lambda Classes
+class Person {
+
+}
+
+class Instructor extends Person {
+
+}
+
+class Student extends Person {
+
+}
+
+class ProjectManager extends Instructor {
+    
+}
